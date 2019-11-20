cd60f2327406c460e0aae914d06b5cecbd71ab57
Beatriz Farkas <42549209+majrnyik@users.noreply.github.com>
2019-11-20 16:14:00 -0300

    Update solucao.py

--- solucao.py
+++ solucao.py
@@ -14,6 +14,7 @@ if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 # initializes environment with ai player using random controller, playing against static enemy
-env = Environment(experiment_name=experiment_name)
+env = Environment(experiment_name=experiment_name,
+				  conctacthurt = "player",
+				  level = 2)
 env.play()
-
